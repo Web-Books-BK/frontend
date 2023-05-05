818d373e5bc7940970b627766fd249515f238d15
nguyenchihieu official <105554125+hieuChiNguyen@users.noreply.github.com>
2023-05-05 09:58:03 +0700

    Loing responsive completed

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,61 +1,22 @@
 import React from "react";
 import {Link } from "react-router-dom";
-import './LoginPage.css'
-
-const Login = {
-    margin: 'auto',
-    height:'400px',
-    width:'400px',
-    border: '2px solid',
-    borderRadius: '20px',
-    backgroundColor: '#B0E0E6',
-    display:'flex',
-    flexDirection: 'column',
-}
-
-const Label = {
-    margin: '10px 20px',
-}
-
-const Input = {
-    margin: '10px 20px',
-    height: '30px',
-}
-
-const Logo = {
-    width: '75px',
-    height: '75px',
-    maxWidth: '100%',
-    maxHeight: '100%',
-}
-
-const Submit = {
-    margin: '20px auto',
-    padding: '5px',
-    width: '100px',
-    cursor: 'pointer',
-}
+import {Box, Button, TextField} from "@mui/material"
 
 export default function LoginPage(){
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log();
-    }
-
     return(
-        <div style={{height: '100vh',padding: '100px', backgroundColor:'pink'}}>
-            <form className="Login" onSubmit={handleSubmit} style={Login}>
-                <div className="logo" style={{margin:'0px auto'}}>
-                    <img src="airbnb.png" alt="LogoAirbnb" style={Logo}/>
-                </div>
-                
-                <label htmlFor="username" style={Label}>Username</label>
-                <input type="text" name="username" className="username" placeholder="enter your username" style={Input}/>
-                <label htmlFor="password" style={Label}>Password</label>
-                <input type="password" name="password" className="password" placeholder="enter your password" style={Input}/>
-                
-                <button type="submit" style={Submit}>Log in</button>
-                <Link to={'/signup'} style={{margin: '0px auto'}}>Đăng ký?</Link>
+        <div>
+            <form>
+                <Box display='flex' flexDirection='column' margin='50px auto' boxShadow='5px 5px 10px #ccc' padding='30px'
+                    width='500px' border='1px solid #ccc' borderRadius='20px' justifyContent='center' alignItems='center'
+                    sx={{':hover': {boxShadow: '10px 10px 20px #ccc'}, 'width': {sm: 300, md: 400, lg: 500}}}>
+                    <div>
+                        <img src='airbnb.png' alt='LogoAirbnb' width='75px' height='75px' maxWidth='100%' maxHeight='100%'/>
+                    </div>
+                    <TextField margin='normal' type='text' variant='outlined' label="Username" sx={{width: {sm: 300, md: 400}}}/>
+                    <TextField margin='normal' type='password' variant='outlined' label="Password" sx={{width: {sm: 300, md: 400}}}/>
+                    <Button sx={{margin:3, borderRadius:2, width: {xs: 200, sm: 300, lg: 150} }} variant='contained' color='info'>Đăng nhập</Button>
+                    <Link to={'/signup'}>Đăng ký?</Link>
+                </Box>
             </form>
         </div>
     );
