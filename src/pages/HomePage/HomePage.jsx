266cfbf2f4a624eb44607a376d1c4239e1be1325
nguyenchihieu official <105554125+hieuChiNguyen@users.noreply.github.com>
2023-05-17 00:20:25 +0700

    login to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Header from "../../components/Header";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('loggedIn')
+        navigate("/login");
+    }
+
     return(
         <div
             style={{
@@ -10,6 +19,8 @@ export default function HomePage() {
         >
             <Header />
             HomePage
+            <Button onClick={handleLogout} sx={{margin:3, borderRadius:2, width: {lg: 100} }} variant='contained' color='info'>Log out</Button>
         </div>
     )
 }
+
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link } from "react-router-dom";
-import {Box, Button, TextField} from "@mui/material"
+import {Box, Button, TextField} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage(){
+    const navigate = useNavigate();
+    const [input, setInput] = useState({
+        username: "",
+        password: "",
+    })
+
+    const handleInput = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value, 
+        })
+    }
+
+    const handleLogin = (e) => {
+        const logged = JSON.parse(localStorage.getItem('user'));
+        if(input.username === logged.username && input.password === logged.password) {
+            localStorage.setItem('loggedIn', true);
+            navigate('/');
+        } else {
+            alert('Something wrong!');
+        }
+
+        e.preventDefault();
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit={handleLogin}>
                 <Box display='flex' flexDirection='column' margin='50px auto' boxShadow='5px 5px 10px #ccc' padding='30px'
                     width='500px' border='1px solid #ccc' borderRadius='20px' justifyContent='center' alignItems='center'
                     sx={{':hover': {boxShadow: '10px 10px 20px #ccc'}, 'width': {sm: 300, md: 400, lg: 500}}}>
                     <div>
-                        <img src='airbnb.png' alt='LogoAirbnb' width='75px' height='75px' maxWidth='100%' maxHeight='100%'/>
+                        <img src='airbnb.png' alt='LogoAirbnb' width='75px' height='75px' sx={{'maxWidth': '100%', 'maxHeight': '100%'}} />
                     </div>
-                    <TextField margin='normal' type='text' variant='outlined' label="Username" sx={{width: {sm: 300, md: 400}}}/>
-                    <TextField margin='normal' type='password' variant='outlined' label="Password" sx={{width: {sm: 300, md: 400}}}/>
-                    <Button sx={{margin:3, borderRadius:2, width: {xs: 200, sm: 300, lg: 150} }} variant='contained' color='info'>Đăng nhập</Button>
+                    <TextField margin='normal' type='text' variant='outlined' label='Username' sx={{width: {sm: 300, md: 400}}}
+                    name='username' value={input.username} onChange={handleInput} />
+                    <TextField margin='normal' type='password' variant='outlined' label='Password' sx={{width: {sm: 300, md: 400}}}
+                    name='password' value={input.password} onChange={handleInput} />
+                    <Button type='submit' sx={{margin:3, borderRadius:2, width: {xs: 200, sm: 300, lg: 150} }} variant='contained' color='info'>Đăng nhập</Button>
                     <Link to={'/signup'}>Đăng ký?</Link>
                 </Box>
-            </form>
+            </form>            
         </div>
+        
     );
 }
